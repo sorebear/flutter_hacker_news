a646ee5bd38db07d917c60a321ac50940b248e83
Soren Baird <sorenbaird@gmail.com>
2019-01-02 17:05:55 -0800

    working on rendering list of Story ids

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
   }
 
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
             return Container(
               padding: EdgeInsets.all(16.0),
               color: index % 2 == 0 ? Colors.grey[100] : Colors.white,
-              child: snapshot.hasData ?
-                Text('I\'m resolved $index') :
-                Text('I haven\'t resolved yet!'),
+              child: snapshot.data[index].hasData ?
+                Text(snapshot.data[index.toString()]) :
+                Text('I\'m still waiting to resolve'),
             );
-          }
+          },
         );
-      },
+        
+      }
     );
-  }
-
-  getFuture() {
-    return Future.delayed(Duration(seconds: 2), () => 'hi');
   }
 }
